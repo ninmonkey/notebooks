d26971e3720a424f68bed02995e9731156d7af2f
Jake Bolton <ninmonkeys@gmail.com>
2022-12-27 16:46:07 -0600

    experimenting with desktop.ini and folders

--- Pwsh/win32_api/Collecting example desktop.ini as templates.ps1
+++ Pwsh/win32_api/Collecting example desktop.ini as templates.ps1
@@ -1,13 +1,73 @@
-# Collecting desktop Template Shell32 files
+@'
+## About
+
+Finding Collecting desktop Template Shell32 files
+
+## core links:
+- [Locating Redirect Registry Strings](https://learn.microsoft.com/en-us/windows/win32/intl/locating-redirected-strings)
+- [Registry-String Formatting / schema](https://learn.microsoft.com/en-us/windows/win32/intl/using-registry-string-redirection#create-a-language-neutral-resource)
+
+    @<PE-path>,-<stringID>[;<comment>]
+    
+- [Prepare Resources for a Shortcut Using INF Format](https://learn.microsoft.com/en-us/windows/win32/intl/using-registry-string-redirection#prepare-resources-for-a-shortcut-using-inf-format)
+
+>  This syntax assumes the use of [StartMenuItems] support from Setup, similar to the syntax used in Syssetup.inf.
+
+    [StartMenuItems]
+    <description> = <binary>,<commandline>,<iconfile>,<iconnum>,<infotip>,<resDLL,resID>
+
+
+> Use the syntax shown below when using INF to add items, for example, an Access Group folder, to the Start menu. This syntax assumes the use of [StartMenuItems] support from Setup, similar to the syntax used in Syssetup.inf.
+
+    [CalcInstallItems]
+    "Name" = %Calc_DESC%
+    "CmdLine" = 11, calc.exe
+    "SubDir" = %Access_GROUP%
+    "WorkingDir" = 11
+
+    "InfoTip" = "@%systemroot%\system32\shell32.dll,-22531"
+
+    "DisplayResource" = "%systemroot%\system32\shell32.dll",22019
+
+## ...
+
+
+## Others / tangents
+
+- maybe more: <https://learn.microsoft.com/en-us/windows/win32/msi/shortcut-table>
+
+
+'@
 
 $Cfg = @{
     DestRoot = gi 'G:\temp\shell32 templates'
 }
 
+$Query ??= @{ }
+$Query.UserProfile3 ??= SearchForIni -PathRoot C:\Users\cppmo_000 -Depth 3
+$Query.UserProfile_all3 ??= SearchForIni -PathRoot C:\Users\cppmo_000 -Depth 3 -All
+
+if ( -not (gcm 'fzf' -ea ignore -CommandType Application )) {
+    $Query.UserSelect ??= $Query.UserProfile_all3 | % Fullname | sort | fzf -m
+}
+else { 
+    'is fzf not in path? '
+    # $Query.UserSelect ??= $Query.UserProfile_all3 | % Fullname | sort | Out-GridView -PassThru
+    $Query.UserSelect ??= $Query.UserProfile_all3 | sort | select Name, Directory | Out-GridView -PassThru | gi 
+}
+
+
 function SearchForIni { 
     <#
     .SYNOPSIS
         local possible desktopini files
+    .notes
+    .future:
+        make it stepp-able? currently it can't stream
+    .example
+        Ps> SearchForIni -PathRoot C:\Users\cppmo_000\SkyDrive\Documents\2021 -Depth 2
+
+        Ps> searchForIni -PathRoot C:\Users\cppmo_000 -Depth 2 -Verbose | Ft Name, FullName
     #>
     [CmdletBinding()]
     param(
@@ -38,7 +98,7 @@ function SearchForIni {
         return $query
     }
 
-    $query = $query | ? Name -eq 'desktop.init'
+    $query = $query | ? Name -eq 'desktop.ini'
     return $query
 }
 
@@ -100,12 +160,12 @@ function FindDesktopIni {
     ) | write-verbose
     mkdir -Path $new_dirName -ea 'ignore'
     
-    Remove-Item -Path $new_iniName  -Force -Confirm
+    Remove-Item -Path $new_iniName  -Force -Confirm -ea 'ignore'
     
     
     
     'Creating: file: label/desktop.ini' | write-verbose
-    Copy-Item -Path $desktopIni? -Destination $new_iniName 
+    $__q = Copy-Item -Path $desktopIni? -Destination $new_iniName 
     # $new_destDir = New-Item -path (Join-Path $SourceItem $ExportLabel) -ItemType Directory -Pass
     # $new_deskItem = Join-Path
     # copy-item -Path $desktopIni? -Destination $NewDestFolder
@@ -121,17 +181,42 @@ function FindDesktopIni {
 
 
 }
+@'
+cached queries, try:
 
-$findDesktopIniSplat = @{
-    Path              = 'C:\Users\cppmo_000\SkyDrive\Documents\2022\dotfiles_git'
-    Verbose           = $true
-    InformationAction = 'Continue'
-    # TestOnly          = $true
-}
+    $targetDir = Get-Item $findDesktopIniSplat.Path
+    $res = FindDesktopIni @findDesktopIniSplat -TestOnly -ea break 
+    $res | ft -auto 
+    hr
+    gci $cfg.DestRoot -Recurse *
+    hr
 
-$targetDir = Get-Item $findDesktopIniSplat.Path
-$res = FindDesktopIni @findDesktopIniSplat -TestOnly -ea break 
-$res | ft -auto 
-hr
-gci $cfg.DestRoot -Recurse *
-hr
+    $Query.Keys
+
+'@ | write-warning 
+
+# . { 
+    # run examples
+    SearchForIni -PathRoot ~ -Depth 1 | ft name, fullname
+
+    $findDesktopIniSplat = @{
+        Path              = 'C:\Users\cppmo_000\SkyDrive\Documents\2022\dotfiles_git'
+        Verbose           = $true
+        InformationAction = 'Continue'
+        # TestOnly          = $true
+    }
+
+    $targetDir = Get-Item $findDesktopIniSplat.Path
+    $res = FindDesktopIni @findDesktopIniSplat -TestOnly -ea break 
+    $res | ft -auto 
+    hr
+    gci $cfg.DestRoot -Recurse *
+    hr
+# }
+write-warning 'remove initial markdown, move to MD doc '
+
+
+throw 'Left off: 
+    Copy the directory itself, to preserve system or whichever properties it has
+'
+
