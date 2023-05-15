9651efc001fba7e4d7ee5d4ebdab32f276dd0292
Jake Bolton <ninmonkeys@gmail.com>
2023-05-14 20:01:43 -0500

    more WhatError records

--- Pwsh/Formatting/2023-05-14 ┐Find Get-Error - WhatErrorRecordFormat.ps1
+++ Pwsh/Formatting/2023-05-14 ┐Find Get-Error - WhatErrorRecordFormat.ps1
@@ -14,16 +14,36 @@ to try JSON serialization
 
 ## Pwsh Error Test
 
-- [Get-Error Implementation](https://github.com/PowerShell/PowerShell/blob/master/src/Microsoft.PowerShell.Commands.Utility/commands/utility/Get-Error.cs)
-- [Get-Error unit tests](https://github.com/PowerShell/PowerShell/blob/master/test/powershell/Modules/Microsoft.PowerShell.Utility/Get-Error.Tests.ps1)
+- [about_format_ps1xml](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_format.ps1xml?view=powershell-7.3)
+- [Format-ExportData](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/export-formatdata?view=powershell-7.3)
+- Implementation:
+    - [Get-Error Implementation](https://github.com/PowerShell/PowerShell/blob/master/src/Microsoft.PowerShell.Commands.Utility/commands/utility/Get-Error.cs)
+    - [Get-Error unit tests](https://github.com/PowerShell/PowerShell/blob/master/test/powershell/Modules/Microsoft.PowerShell.Utility/Get-Error.Tests.ps1)
+    -
 
 
-It's declared as returning:
-    - [System.Management.Automation.ErrorRecord#PSExtendedError]
-    - [System.Exception#PSExtendedError]
+It's declared as returning / involving:
 
-    - [sma.ErrorRecord#PSExtendedError]
+## Short names
+
+    - [Exception]
     - [Exception#PSExtendedError]
+    - [sma.ErrorRecord]
+    - [sma.ErrorRecord#PSExtendedError]
+
+
+## Same as full names
+
+    - [System.Exception]
+    - [System.Exception#PSExtendedError]
+    - [System.Management.Automation.ErrorRecord]
+    - [System.Management.Automation.ErrorRecord#PSExtendedError]
+
+## Interal cs
+
+    internal const string ErrorRecordPSExtendedError = "System.Management.Automation.ErrorRecord#PSExtendedError";
+    internal const string ExceptionPSExtendedError = "System.Exception#PSExtendedError";
+
 '@
 
 Push-Location $PSScriptRoot
