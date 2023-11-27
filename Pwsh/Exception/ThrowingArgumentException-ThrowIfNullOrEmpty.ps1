cee95457c412994550d74d0db251606a86775eea
Jake Bolton <ninmonkeys@gmail.com>
2023-11-27 06:52:40 -0600

    added ThrowIfNullOrEmpty

---
+++ Pwsh/Exception/ThrowingArgumentException-ThrowIfNullOrEmpty.ps1
@@ -0,0 +1,20 @@
+﻿
+function NewUser {
+    [Cmdletbinding()]
+    param(
+    [string]$UserName )
+
+    [ArgumentException]::ThrowIfNullOrEmpty(
+        <# argument: #> $UserName, <# paramName: #> 'UserName')
+
+    [ArgumentException]::ThrowIfNullOrWhiteSpace(
+        <# argument: #> $UserName, <# paramName: #> 'UserName')
+}
+
+NewUser 'Bob'
+try {
+    newUser ''
+} catcH {
+    'bad stuff'
+}
+# NewUser ''
