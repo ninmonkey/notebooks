2e0e86387212d6e2560a3e0797db27a6c6b27df3
Jake Bolton <ninmonkeys@gmail.com>
2023-12-03 16:20:26 -0600

    ValidateNotNullOrWhitespace

---
+++ Pwsh/validation/OptionalParam.Using.ValidateNotWhiteSpace.ps1
@@ -0,0 +1,20 @@
+﻿function TestOption {
+    param(
+        [ValidateNotNullOrWhiteSpace()]
+        [Parameter()]
+        [string[]]$FilterVersion,
+
+        [Parameter(Mandatory)]
+        $Name
+    )
+
+    $PSBoundParameters | Json | Write-Host -bg orange -fg black
+}
+
+TestOption -f 'a', 'b' -n 'bob' # is good
+TestOption -n 'bob'             # is good.
+TestOption -f '' -n 'bob'       # throws
+TestOption -f $Null -n 'bob'    # throws
+TestOption -f @() -n 'bob'      # throws
+TestOption -f @('') -n 'bob'    # throws
+TestOption -f @('a') -n 'bob'   # is good
