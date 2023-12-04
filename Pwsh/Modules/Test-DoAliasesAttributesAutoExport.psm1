07dd168c32950e1534219bb40cf3c63d61d34e6d
Jake Bolton <ninmonkeys@gmail.com>
2023-12-04 08:11:23 -0600

    alias export test

---
+++ Pwsh/Modules/Test-DoAliasesAttributesAutoExport.psm1
@@ -0,0 +1,17 @@
+﻿'Testing: Do aliases auto-export if the command they are attributes to, are? Nope.' | write-host
+
+function InternalFunc {
+    [alias('Ini.Func')]
+    param()
+    $MyInvocation.MyCommand.Name
+}
+function Alice.TestExternal {
+    [alias(
+        'Ini.Exter',
+        'Alice.Exter'
+    )]
+    param()
+    $MyInvocation.MyCommand.Name
+}
+
+Export-ModuleMember -Function @('Alice.*') -Alias ('Alice.*') -Variable @('Alice.*')
