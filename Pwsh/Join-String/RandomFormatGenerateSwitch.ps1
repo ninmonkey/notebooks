9b71b28208621933d779a33bb19bd28854e41b4f
Jake Bolton <ninmonkeys@gmail.com>
2023-12-05 17:43:47 -0600

    random switch script

---
+++ Pwsh/Join-String/RandomFormatGenerateSwitch.ps1
@@ -0,0 +1,36 @@
+﻿
+
+
+$records = @(
+    [pscustomobject]@{
+        Name = 'POST JURY FEE'
+        Path = '\PLEADINGS'
+    }
+    [pscustomobject]@{
+        Name = 'MTS'
+        Path = '\PLEADINGS\DEMURRER-STRIKE\STRIKE'
+
+    }
+)
+$T = @{}
+$T.Name = @'
+    '\s{0}[\s_]' {{
+'@
+$T.Path = @'
+        Join-Path $FOLDER '{0}' ; break
+    }}
+'@
+$T.Suffix = @'
+}
+'@
+$T.Prefix = @'
+$folder = switch -Regex ($_.Name) {
+'@
+
+$T.Prefix
+$records | %{
+    $Item = $_
+    $T.Name -f $Item.Name
+    $T.Path -f $Item.Path
+}
+$T.Suffix
