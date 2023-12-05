b0dc0f3df3f963a85738e130e42be06d3d11fae6
Jake Bolton <ninmonkeys@gmail.com>
2023-12-05 10:43:31 -0600

    updated test

--- Pwsh/Parameters/Using-ThrowIfNullOrWhiteSpace-OnObjects.ps1
+++ Pwsh/Parameters/Using-ThrowIfNullOrWhiteSpace-OnObjects.ps1
@@ -1,11 +1,13 @@
 ﻿function FormatDate {
+    [Alias('Test.Fmt.Date')]
     [CmdletBinding()]
     param(
         [Parameter(Mandatory, ValueFromPipeline)]
             [datetime]$Dt,
 
         [Parameter()]
-            [ArgumentCompletions('([CultureInfo]::InvariantCulture)', 'en-us', 'de-de' )]
+            [ArgumentCompletions(
+                '([CultureInfo]::InvariantCulture)', 'en-us', 'de-de' )]
             $Culture
             # $Culture = [CultureInfo]::InvariantCulture
             # [CultureInfo]$Culture = [CultureInfo]::InvariantCulture
@@ -13,13 +15,15 @@
     $Culture ??= [CultureInfo]::InvariantCulture
     if( [String]::IsNullOrEmpty( $Culture )) { throw "MandatoryCultureWasBlank!" }
     [ArgumentException]::ThrowIfNullOrWhiteSpace( $Culture, 'Culture' )
-
     return $Dt.ToString( $Culture )
 
     # $PSBoundParameters | ConvertTo-Json -Depth 1 -Compress | Write-debug
     # $null -eq $Culture | Join-String -f 'Cult == $Null: {0}' | write-verbose -verb
 }
+FormatDate -Dt (get-date) (get-culture 'en-us') # good
+FormatDate -Dt (get-date)                       # error
 
+return
 FormatDate (Get-Date) -Culture (Get-Culture 'en-us')
 return
 FormatDate (Get-Date) -Culture ([CultureInfo]::InvariantCulture)
