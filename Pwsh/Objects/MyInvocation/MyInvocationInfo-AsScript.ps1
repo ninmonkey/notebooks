4872b50d9b88d919de65e54cdeb2f01b12795aca
Jake Bolton <ninmonkeys@gmail.com>
2023-12-07 07:32:02 -0600

    new: MyInvocationInfoSummary

---
+++ Pwsh/Objects/MyInvocation/MyInvocationInfo-AsScript.ps1
@@ -0,0 +1,31 @@
+﻿using namespace System.Collections.Generic
+using namespace System.Management.Automation
+using namespace System.Management
+
+$Config = @{
+    SuperVerboseSummaryRender = $True
+}
+if($SuperVerbose) { }
+$meta = [ordered]@{
+    PsRoot       = $PSScriptRoot
+    PsCmdPath    = $PSCommandPath
+    MyI          = $MyInvocation
+    MyI_Cmd      = $MyInvocation.MyCommand # [ScriptInfo]
+    MyI_Cmd_Path = $MyInvocation.MyCommand.Source
+    MyI_Cmd_Src  = $MyInvocation.MyCommand.Source
+
+    # $MyInvocation.MyCommand.Name
+    # $MyInvocation
+    # $MyInvocation.DisplayScriptPosition
+
+}
+$meta | ft -auto
+
+
+if($Config.SuperVerboseSummaryRender) {
+    $Meta.GetEnumerator() | %{
+        h1 ($_.Key |Join-String -f '$meta[ {0} ] => ...')
+        if( $_.Value -is 'string' ) { $_.Value |Join-String -op 'raw [string] =: '}
+        else { $_.Value | iot2 -NotSkipMost | ft -auto }
+    }
+}
