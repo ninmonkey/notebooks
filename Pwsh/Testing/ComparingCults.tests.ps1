2782b9d7fe7b6b27c163cfe632bd86bb88e5e172
Jake Bolton <ninmonkeys@gmail.com>
2023-12-18 12:09:01 -0600

    pester example using cults

---
+++ Pwsh/Testing/ComparingCults.tests.ps1
@@ -0,0 +1,25 @@
+﻿describe 'thing' {
+    It '<Render> is <Expected>' -foreach @(
+        @{
+            In = 1000
+            Expected = '1,000'
+            FStr = 'n0'
+            Cult = 'en-us'
+        }
+        @{
+            In = 1000
+            Expected = '1,000.00'
+            FStr = 'n'
+            Cult = 'en-us'
+        }
+        @{
+            In = 1000
+            Expected = '1.000,0'
+            FStr = 'n1'
+            Cult = 'de-de'
+        }
+    ) -Test {
+        $cult = Get-Culture $Cult
+        $In.ToString($FStr, $Cult) | Should -BeExactly $Expected
+    }
+}
