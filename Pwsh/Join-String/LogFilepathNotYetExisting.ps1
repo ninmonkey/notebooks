d642898ce35067cf3612e999684fd28f3afaed5b
Jake Bolton <ninmonkeys@gmail.com>
2023-12-25 09:23:02 -0600

    a

---
+++ Pwsh/Join-String/LogFilepathNotYetExisting.ps1
@@ -0,0 +1,13 @@
+﻿
+
+
+@(
+    'temp:\notyetexisting.log'
+
+    Join-path (gi 'temp:') 'notyetexisting.log'
+    Join-path (Convert-Path 'temp:') 'notyetexisting.log'
+
+    # Can you do this? Yes. Should you? Probably not.
+    [IO.FIleInfo]::new('temp:\notyetexisting.log')
+    [IO.FIleInfo]::new( (Join-Path (gi temp:\) 'notyetexisting.log'))
+ ) | Join-String -f "`n - {0}"
---
+++ Pwsh/Join-String/RandomFormatGenerateSwitch-UsingJoinString.ps1
@@ -0,0 +1,39 @@
+﻿# using join string instead
+$T = @{}
+$T.Name = @'
+    '\s{0}[\s_]' {{
+'@
+$T.Path = @'
+        Join-Path $FOLDER '{0}' ; break
+    }}
+'@
+$T.Suffix = @'
+}
+'@
+$T.Prefix = @'
+$folder = switch -Regex ($_.Name) {
+'@
+
+function Join.Prefix {
+    param(
+        [int]$Depth = 1
+    )
+    process {
+        $Line = $_
+        $prefix = '    ' * $depth -join ''
+        Join-String -f "$Prefix{0}" -in $Line
+    }
+}
+@(
+    $T.Prefix
+    $records | %{
+        $Item = $_
+        $T.Name -f $Item.Name
+        $T.Path -f $Item.Path
+    }
+    $T.Suffix
+) |
+
+
+
+return
