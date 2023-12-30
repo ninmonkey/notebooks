8d9fc4f26476dcf57de3d9c9c43d66dff42c13ab
Jake Bolton <ninmonkeys@gmail.com>
2023-12-30 11:51:43 -0600

    new example: attribute list

---
+++ Pwsh/Attributes/UsingAttributeLists.ps1
@@ -0,0 +1,66 @@
+﻿using namespace System
+using namespace System.Collections.Generic
+using namespace System.Reflection
+using namespace System.Management
+using namespace System.Management.Automation
+
+err -Clear
+Get-Module Picky -all | Remove-Module
+$modPath = 'H:\data\2023\pwsh\PsModules\Picky\Picky\Picky.psm1'
+# import-module Picky -force -PassThru
+import-module $ModPath -force -PassThru
+
+@'
+# see more
+
+- [Creating Custom Attributes](https://powershell.one/powershell-internals/attributes/custom-attributes#a-better-autolearn-attribute)
+- [Retrieving Information Stored in Attributes](https://learn.microsoft.com/en-us/dotnet/standard/attributes/retrieving-information-stored-in-attributes)
+
+## Summary:
+
+- for mandatory parameters:
+  - are *always* positional
+  - are c-tor arguments
+
+- for optional positional parameters:
+  - add a parameterless c-tor
+  - or a c-tor overload that omits the parameter to be optional
+
+- for optional named parameters:
+  - implemented as properties not in a c-tor
+
+'@ | Write-Host -fore 'green'
+class AuthorAttribute : Attribute {
+    [List[string]]$Authors = @()
+    AuthorAttribute( [string[]]$Authors ) {
+        $this.Authors = $Authors
+    }
+    AuthorAttribute() {}
+
+}
+function DoWork {
+    [AuthorAttribute(Authors = ('cat', 'monkey', '🐒'))]
+    param(
+        [string]$Text
+    )
+    'Did stuff'
+}
+
+
+DoWork
+h1 '.ResolveParameter(Text)'
+(gcm DoWork).ResolveParameter('Text')
+
+h1 '.ResolveParameter(Text).Attributes'
+(gcm DoWork).ResolveParameter('Text').Attributes
+
+h1 '.ScriptBlock.Attributes'
+[List[Attribute]]$Attrs = (gcm DoWork).ScriptBlock.Attributes
+$authorAttr = $Attrs[0]
+
+
+(gcm DoWork) | Function.GetInfo Attributes
+
+$target = gcm 'DoWork'
+# Picky\Function.GetInfo $target
+# Function.GetInfo -Fn $Target -Out Attributes
