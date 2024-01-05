7fc33a1e5c5e5b4ccf546253e7ed2658aa495102
Jake Bolton <ninmonkeys@gmail.com>
2024-01-05 13:47:44 -0600

    new pwsh module demo

---
+++ Pwsh/Modules/BasicModuleScope.psm1
@@ -0,0 +1,21 @@
+﻿$script:Cache = @{}
+$script:DefaultUser = $Null
+function Get-NinModules {
+    # caches module listing
+    param()
+    $state = $script:cache
+    if( -not $state.ContainsKey('AllModules') ) {
+        'not cached, please wait....' | write-host -fore 'yellow'
+        $state.AllModules = Get-Module -ListAvailable | Sort-Object
+    }
+    return $state.AllModules
+}
+
+function Set-NinUser {
+    param( [string]$Username)
+    $script:DefaultUser = $UserName
+}
+
+Export-moduleMember -Function @(
+    '*-Nin*'
+)
