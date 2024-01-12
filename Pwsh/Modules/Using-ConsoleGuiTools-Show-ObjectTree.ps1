5456f84f83c433678fd322562f48eb8448662ae7
Jake Bolton <ninmonkeys@gmail.com>
2024-01-12 15:42:31 -0600

    new links

---
+++ Pwsh/Modules/Using-ConsoleGuiTools-Show-ObjectTree.ps1
@@ -0,0 +1,30 @@
+﻿using namespace System.Collections.Generic
+
+class Node {
+    [string]$Name = 'node'
+    [List[Object]]$Children = @()
+}
+
+$tree = @(
+    [Node]@{
+        Name = 'Root'
+        Children = @(
+            [Node]@{
+                Name = 'style'
+                Children = [Node]@{
+                    Name = 'SomeFont'
+                }
+            }
+            [Node]@{
+                Name = 'body'
+                Children = @(
+                    [Node]@{
+                        Name = 'flex-item'
+                    }
+                )
+            }
+        )
+    }
+)
+
+$tree | Show-ObjectTree
