d97cb0ad4273144ee7d0c7d20492962b3e4d954b
Jake Bolton <ninmonkeys@gmail.com>
2024-02-05 14:49:50 -0600

    Update Cult Info Intl.js

--- js/dates/Cult Info Intl.js
+++ js/dates/Cult Info Intl.js
@@ -1,5 +1,5 @@
 // file:///H:\data\2023\pwsh\notebooks\js\dates\Cult%20Info%20Intl.js
-// cult info intl8.js
+// about: cult info intl8.js
 // some of the code is weird, because it's meant for an interactive "Qoukka.js" notebook
 
 let cultInfo = Intl.DateTimeFormat('en-US')
@@ -17,7 +17,7 @@ d0;
 d0 = new Date(2020, 1, 0) // jan 31 2020
 d0;
 
-let samples_0 = [
+let samples = [
     { y: 2020, m: 1, d: 1 }, // feb  1 2020
     { y: 2020, m: 0, d: 0 }, // feb  1 2020
     { y: 2020, m: 0, d: 1 }, // feb  1 2020
@@ -34,8 +34,65 @@ parts;
 _ = JSON.stringify( parts )
 _;
 
+function ObjectFromParts ( objectList ) {
+    return Array.from( objectList ).reduce( (accum, part) => {
+        return {
+            ...accum,
+            [part.type]: part.value
+        }
+    }, {})
+}
+
+_ = parts.reduce( (obj, item) => {
+    // obj[ item.key ] = item.value
+    // return obj
+    const k = item.type
+    const value = item.value
+    return {
+        ...obj,
+        [item.type]: item.value
+    }
+}, {} )
+_
+
 let fin = parts.reduce( (acc, part) => { return `_${ acc } ; ${ part.value }` } )
 fin;
 
 _ = parts.reduce( (acc, part) => { return `_${ acc } ; ${ part.value }` } )
+_
+
+_ = samples[0]
+_
+
+_ = ObjectFromParts( parts )
+_;
+
+const arr = [{
+  key: "11",
+  value: "1100"
+}, {
+  key: "22",
+  value: "2200"
+}];
+const object = arr.reduce((obj, item) => ({
+  ...obj,
+  [item.key]: item.value
+}), {});
+console.log(object)
+
+arr.reduce( (obj, item) => {
+    obj[ item.key ] = item.value
+    return obj
+}, {} )
+
+arr.reduce( (obj, item) => {
+    obj[ item.key ] = item.value
+    return obj
+}, {} )
+
+arr;
+// function intlPartsToObject {
+
+// }
+hash = {}
 
