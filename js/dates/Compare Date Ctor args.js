e616a79c7442fd7798799cd047d5c88e6603af3f
Jake Bolton <ninmonkeys@gmail.com>
2024-02-05 15:07:34 -0600

    Update Compare Date Ctor args.js

--- js/dates/Compare Date Ctor args.js
+++ js/dates/Compare Date Ctor args.js
@@ -3,7 +3,9 @@
 // some of the code is weird, because it's meant for an interactive "Qoukka.js" notebook
 
 // test 1: as integers
+const now = new Date()
 let d0 = new Date(2020, 1, 1) // feb  1 2020
+let _ = d0;
 d0;
 d0 = new Date(2020, 0, 1) // jan  1 2020
 d0;
@@ -12,7 +14,14 @@ d0;
 d0 = new Date(2020, 1, 0) // jan 31 2020
 d0;
 
-let samples_0 = [
+let cultName = 'en-us'
+let cult = new Intl.DateTimeFormat( cultName )
+_ = cult.formatToParts( now )
+_;
+_ = _[0]
+_;
+
+let samples = [
     { y: 2020, m: 1, d: 1 }, // feb  1 2020
     { y: 2020, m: 0, d: 0 }, // feb  1 2020
     { y: 2020, m: 0, d: 1 }, // feb  1 2020
@@ -22,34 +31,33 @@ let samples_0 = [
     { y: 2020, m: '0', d: '1' },
     { y: 2020, m: '1', d: '0' },
 ]
-let summary_a = Array.from([])
-samples_0.forEach(
+let summary = Array.from([])
+samples.forEach(
     (o) => {
         const one = new Date( o.y, o.m, o.d )
-        summary_a.push( {
-            y: one.getFullYear(),
-            m: one.getMonth(),
-            d: one.getDate(),
+        summary.push( {
+            y: o.y,
+            m: o.m,
+            d: o.d,
+            obj_y: one.getFullYear(),
+            obj_m: one.getMonth(),
+            obj_d: one.getDate(),
             result: one
         } )
     }
 )
 
-// summary_a;
+// summary;
 
-q = summary_a[0]
+_ = summary[0]
+_
+_ = summary[1]
+_
+q = summary[1]
 q;
 
-q = summary_a[1]
-q;
 
-let cult = new Intl.DateTimeFormat('en-us')
-let cultName = 'en-us'
-let samples = [
-    { y: 2020, m: 1, d: 1 }, // feb  1 2020
-    { y: 2020, m: 0, d: 1 }, // feb  1 2020
-]
-let summary = Array.from([])
+
 function formatToParts( dateObj, options ) {
     const cultDefaults = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
     options = {
@@ -63,61 +71,10 @@ function formatToParts( dateObj, options ) {
 
 // let parts = cultInfo.formatToParts( new  Date(2000, 1, 3))
 
-const arr = [{
-  key: "11",
-  value: "1100"
-}, {
-  key: "22",
-  value: "2200"
-}];
-const object = arr.reduce((obj, item) => ({
-  ...obj,
-  [item.key]: item.value
-}), {});
-console.log(object)
+_ = summary[0]
+_
+_ = summary[1]
+_
 
-arr.reduce( (obj, item) => {
-    obj[ item.key ] = item.value
-    return obj
-}, {} )
-
-arr.reduce( (obj, item) => {
-    obj[ item.key ] = item.value
-    return obj
-}, {} )
-
-arr;
-// function intlPartsToObject {
-
-// }
-hash = {}
-parts.map((o) => hash[ o.type ] = o.value )
-
-samples.forEach(
-    (o) => {
-        const dtObj = new Date( o.y, o.m, o.d )
-        summary.push( {
-            param_y: o.y,
-            param_m: o.m,
-            param_d: o.d,
-            obj_y: dtObj.getFullYear(),
-            obj_m: dtObj.getMonth(),
-            obj_d: dtObj.getDate(),
-            // result: dtObj,
-            // fmt: formatToParts( dtObj )
-        } )
-    }
-)
-
-
-
-// summary;
-
-q = summary[0]
-q;
-
-q = summary[1]
-q;
-
-dive = summary[0].obj_
+dive = summary[0]
 dive;
