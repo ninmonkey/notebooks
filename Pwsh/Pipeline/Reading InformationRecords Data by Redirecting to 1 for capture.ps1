1e34e529e5f9cde3c4f94977d7df507bf3a005ac
Jake Bolton <ninmonkeys@gmail.com>
2024-04-30 00:37:13 -0500

    Create Reading InformationRecords Data by Redirecting to 1 for capture.ps1

---
+++ Pwsh/Pipeline/Reading InformationRecords Data by Redirecting to 1 for capture.ps1
@@ -0,0 +1,60 @@
+﻿Function tryIter {
+    [CmdletBinding()] param()
+    # write-verbose -verbose 'hi'
+    Write-Information 'world' -infa Continue
+}
+if($false) {
+    tryIter *>&1 -Verbose | %{
+        $color = switch( $_ ) {
+            { $_ -is [InformationRecord] } { 'red'    }
+            { $_ -is [VerboseRecord] }     { 'yellow' }
+            default { 'purple' }
+        }
+        $_ | New-text -fg $color
+    } | Join-string
+}
+
+function fakeIC {
+    [CmdletBinding()]
+    param()
+    $Verby = [VerboseRecord]::new('something verbosy')
+    $verby.PipelineIterationInfo
+    $source = 'JBTransport'
+    $data = @{
+        OriginalVerboseRecord = $verby
+        user = @{ name = 'jen' ; id = 24; }
+        species = 'cat'
+        Source = $Source
+    }
+    [string[]]$tags = @('RedirectedVerbose')
+
+    $ir = [InformationRecord]::new( $data, $source )
+    $ir.Tags.AddRange( $tags )
+
+    # $PSCmdlet.WriteVerbose( $verby.Message )
+    $PsCMdlet.WriteInformation(<# messageData: #> $data, <# tags: #> $tags )
+    $PsCMdlet.WriteInformation( $ir )
+}
+    # $ir = [InformationRecord]::new( @{
+    #         OriginalVerboseRecord = $verby
+    #         user = @{ name = 'jen' ; id = 24; }
+    #         species = 'cat'
+    #     },
+
+# fakeIc
+
+fakeIC *>&1 -Verbose -infa 'Continue' | %{
+    if(-not $_) { return }
+    $color = switch( $_ ) {
+        { $_ -is [InformationRecord] } {
+            $_.MessageData.OriginalVerboseRecord.GetType() | out-host
+            $_.MessageData | out-host
+            $_.MessageData.OriginalVerboseRecord|json | out-host
+
+            'red' # put a breakpoint here
+        }
+        { $_ -is [VerboseRecord] }     { 'yellow' }
+        default { 'purple' }
+    }
+    $_ | New-text -fg $color
+} | Join-string
