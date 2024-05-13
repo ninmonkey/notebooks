c507238524fc04f881b9f429851ba3c2494eed5b
Jake Bolton <ninmonkeys@gmail.com>
2024-05-13 18:43:44 -0500

    more notes

--- Pwsh/Unicode/UtilsForUnicodeStrings.psm1
+++ Pwsh/Unicode/UtilsForUnicodeStrings.psm1
@@ -33,6 +33,37 @@ function ShowHex {
 }
 
 $script:Enc8  = [Text.Encoding]::GetEncoding('utf-8')
-$script:Enc16 = [Text.Encoding]::GetEncoding('utf-8')
+$script:Enc16 = [Text.Encoding]::GetEncoding('utf-16le')
+
+@'
+## Try:
+
+$enc8 and $enc16 are shorthand for using that specific encoding
+
+$InputText = '🐒'
+$bytes_As8 = $enc8.GetBytes( $InputText )
+    # value:  240, 159, 144, 146
+
+$str       =
+    $enc8.GetString(
+        $enc8.GetBytes( $InputText )
+    )
+
+$str -eq $InputText
+    # true. it's around trip!
+
+( When you remove module, it will remove the variables that were exported )
+
+We care about two functions on [Text.Encoding]
+
+GetBytes:
+    input: String, output: Bytes
+    this takes text, converting them to their bytes representation for this specific encoding
+GetString:
+    input: Bytes, Output: String
+    this takes bytes and converts them back into strings
+
+'@ | write-verbose -verbose
+
 
 Export-ModuleMember -Function @( '*' ) -Alias @( '*') -Variable @('Enc*')
