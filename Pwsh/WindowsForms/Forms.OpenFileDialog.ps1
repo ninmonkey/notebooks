643b44d23a57b537e3dc1ee9bb66c2417b1c5020
Jake Bolton <ninmonkeys@gmail.com>
2024-05-14 23:22:21 -0500

    demo: Open file dialog

---
+++ Pwsh/WindowsForms/Forms.OpenFileDialog.ps1
@@ -0,0 +1,20 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+[Windows.Forms.OpenFileDialog]$Dlg = @{
+    InitialDirectory = get-item $PSScriptRoot
+    Title = 'select file to upload'
+    Filter = 'ZIP files|*.zip|Text Documents|*.txt|Shell Scripts|*.*sh|All Files|*.*'
+    FilterIndex = 4
+
+    AddToRecent      = $false
+    CheckFileExists  = $true
+    CheckPathExists  = $true
+    Multiselect      = $false
+    ShowHelp         = $true
+    ShowHiddenFiles  = $false
+    ShowPinnedPlaces = $true
+    ShowPreview      = $true
+}
+
+$Dlg.ShowDialog() | Out-null
+'You chose file[s]: {0}' -f $Dlg.FileNames -join ', '
