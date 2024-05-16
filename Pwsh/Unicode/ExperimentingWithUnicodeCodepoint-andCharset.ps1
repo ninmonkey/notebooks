3b89aa032232f7d21c9b3841f6bab8f6325dfc5a
Jake Bolton <ninmonkeys@gmail.com>
2024-05-15 21:06:34 -0500

    first pass of EncodingCodepoint

---
+++ Pwsh/Unicode/ExperimentingWithUnicodeCodepoint-andCharset.ps1
@@ -0,0 +1,47 @@
+﻿# preview a list of possible Encoding class names
+[Text.Encoding]::GetEncodings().ForEach({ $_.GetEncoding().GetType() }) | Sort-object -Unique FullName|ft
+
+# Grab the windows ones
+( $enc_winX =
+    [Text.Encoding]::GetEncodings().
+        Where({ $_.CodePage -match '125\d+' }) )
+| ft
+
+$enc_winX.GetEncoding()|Ft
+
+
+
+$Rune = 'ë'
+# return an object of before and after encoding the character for ervery encoding
+
+
+# $_      -is [EncodingInfo]
+# $curEnc -is [Encoding]
+$query = $enc_winX.ForEach({
+    $curEnc = $_.GetEncoding()
+
+
+    $Bytes  = $curEnc.GetBytes( $Rune )
+
+    $info = [ordered]@{
+        Enc_Codepage = $_.CodePage
+        Enc_Name     = $_.Name
+        Enc_Display  = $_.DisplayName
+        Bytes        = $Bytes
+        BytesHex     = $Bytes
+            | Join-String -f '{0:x}' -sep ' '
+
+        Before      = $Rune
+    }
+
+
+
+    #     Enc_        = $encCur.CodePage
+    #     Enc_Type    = $encCur.GetType().fullname
+
+
+
+    # }
+})
+
+$query | Ft -auto
